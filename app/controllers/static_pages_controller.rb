e64b73ff7e7dbe58d6bf4f844330f526cf7051a4
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2022-12-04 20:10:50 +0100

    Show a user's photos after their id is typed in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,13 @@
 class StaticPagesController < ApplicationController
+    require 'flickr'
+
     def index
+        flickr = Flickr.new(ENV["flickr_key"], ENV["flickr_secret"])
         
+        if(params[:user_id])
+            @photos = flickr.photos.search(user_id: params[:user_id])
+        else
+            @photos = flickr.photos.getRecent
+        end
     end
 end
